0b9d3242f660a2991e412346021a1cac34733cdf
susan <susan@helium.(none)>
2012-08-29 09:17:44 -0500

    Deleted accidentally added pictures

--- inst/CompareTilingGraphs.R
+++ inst/CompareTilingGraphs.R
@@ -17,7 +17,7 @@ LossTable <- NULL
 
 bins <- rbind(c(1,1), c(2,2), c(2,5), c(5, 2), c(4,4), c(3, 5), c(5, 3), c(5, 5), c(5, 25), c(25, 5), c(2, 50), c(50, 2), c(5, 50))
 
-setwd("/home/susan/Dropbox/dbaccess/NewWriteUp")
+setwd("/home/susan/Dropbox/GraphicsGroup/dbaccess/NewWriteUp")
 computeLoss <- function(binning, data){
   lRandom <- lossRandom(data, binning=binning, newData = TRUE)
   lMean <- lossMean(data, binning=binning, newData=TRUE)
